ae7dac7bdc18ceccfd088d8ba85d859be1212ee2
geri <gerandi1997@gmail.com>
2023-02-13 16:46:16 +0100

    styling

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -4,28 +4,25 @@ import { useState } from 'react';
 import './Login.scss'
 import axios from 'axios';
 const Login = () => {
-    const [username,setUsername] = useState("");
-    const [password,setPassword] = useState();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState();
     const user = {
-        username:username,
-        password:password
+        username: username,
+        password: password
     }
     const LoginHandle = () => {
-        axios.post('http://localhost:8000/login',user).then(res => localStorage.setItem('jwt','124q3cdfgdraw3q244444w555cfgudtse57w34s5eu8cfise58'))
-     }
+        axios.post('http://localhost:8000/login', user).then(res => localStorage.setItem('jwt', '124q3cdfgdraw3q244444w555cfgudtse57w34s5eu8cfise58'))
+    }
     return (
         <div className='login-container'>
             <h1>Login</h1>
             <br></br>
-<label>Username :            <input className='inputforms' type='text'  onChange={(e) => {setUsername(e.target.value)}} ></input>
-</label>            <br></br><br></br>
-<label>Password :<input className='inputforms' type='password'  onChange={(e) => {setPassword(e.target.value)}} ></input>
-
-    </label> 
-    <p style={{marginTop:"3em"}}>Not a user? No problem you can <a className='rg-here' href='/register'>register here</a> </p>
-    <br></br>
-    <button onClick={LoginHandle} class="btn">Login</button>
-       </div>
+            <label>Username :<input className='inputforms' type='text' onChange={(e) => { setUsername(e.target.value) }} ></input></label>
+            <label>Password :<input className='inputforms' type='password' onChange={(e) => { setPassword(e.target.value) }} ></input></label>
+            <p style={{ marginTop: "3em" }}>Not a user? No problem you can <a className='rg-here' href='/register'>register here</a> </p>
+            <br></br>
+            <button onClick={LoginHandle} class="btn">Login</button>
+        </div>
     );
 };
 
--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { useState } from 'react';
 const Signup = () => {
-    const [username,setUsername] = useState("");
-    const [fullname,setFullName] = useState("");
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState();
+    const [username, setUsername] = useState("");
+    const [fullname, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState();
     return (
         <div className='login-container'>
             <h1>Register</h1>
-            <label>Username :            <input className='inputforms' type='text'  onChange={(e) => {setUsername(e.target.value)}} ></input>
-</label>            <br></br><br></br>
-<label>Full Name :<input className='inputforms' type='text'  onChange={(e) => {setFullName(e.target.value)}} ></input></label><br></br><br></br>
-<label>Email :<input className='inputforms' type='text'  onChange={(e) => {setEmail(e.target.value)}} ></input></label><br></br><br></br>
-<label>Password :<input className='inputforms' type='password'  onChange={(e) => {setPassword(e.target.value)}} ></input>
-
-    </label> <br></br><br></br>
-    <button class="btn">Signup</button>
+            <label>Username :<input className='inputforms' type='text' onChange={(e) => { setUsername(e.target.value) }} ></input></label>
+            <label>Full Name :<input className='inputforms' type='text' onChange={(e) => { setFullName(e.target.value) }} ></input></label>
+            <label>Email :<input className='inputforms' type='text' onChange={(e) => { setEmail(e.target.value) }} ></input></label>
+            <label>Password :<input className='inputforms' type='password' onChange={(e) => { setPassword(e.target.value) }} ></input></label>
+            <button class="btn">Signup</button>
         </div>
     );
 };
