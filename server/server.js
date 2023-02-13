ca0fa38b824e521c18a31af4380366eae98178bd
OlviXhaferaj <olvixhaferaj@gmail.com>
2023-02-13 22:06:23 +0100

    login register logout

--- server/server.js
+++ server/server.js
@@ -2,10 +2,26 @@ require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const app = express();
+const cookieParser = require('cookie-parser');
 
 // middleware
-app.use(cors());
+app.use(cors({
+    //adding the ability to use credentials with cookies
+    credentials:true
+}));
 app.use(express.json());
+
+// configure the server to accept and update the cookies
+app.use(cookieParser());
 
 // mongoDB initialization
 require("./config/db.config");
+
+//importing user routes
+require('./routes/user.routes')(app);
+
+const PORT = 8000;
+
+app.listen(PORT, function () {
+    console.log(`The server has started on PORT: ${PORT}`)
+})
