411e6b08271f02fa863409a59b5a634c3854441e
geri <gerandi1997@gmail.com>
2023-02-17 11:17:20 +0100

    FavoriteButton fix

--- client/src/components/favorite/FavoriteButton.js
+++ client/src/components/favorite/FavoriteButton.js
@@ -2,51 +2,26 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./favorite-button.scss";
 
-<<<<<<< HEAD
 function FavoriteButton(props) {
   const [favorite, setFavorite] = useState(false);
   const [token, setToken] = useState(localStorage.getItem("jwt"));
 
-=======
-function FavoriteButton({ movieId }) {
-  const [isLoading, setLoading] = useState(false);
-  const [isFavorite, setFavorite] = useState(false);
-  const [token,setToken] = useState(localStorage.getItem("jwt"))
-  
- 
->>>>>>> 7b15585d5ca8af2713bf6cc3b7efcea227d1c2cc
   useEffect(() => {
     async function fetchData() {
       try {
-<<<<<<< HEAD
         const response = await axios.get(`http://localhost:8000/api/favorites/${props.movieId}`);
         setFavorite(response.data);
-=======
-        const response = await axios.get(`/api/favorites/${movieId}`);
-        setFavorite(response.data.isFavorite);
->>>>>>> 7b15585d5ca8af2713bf6cc3b7efcea227d1c2cc
       } catch (error) {
         console.error(error);
       }
     }
     fetchData();
-<<<<<<< HEAD
   }, [props.movieId]);
 
   const addFavorite = async () => {
     try {
       const response = await axios.post(`http://localhost:8000/api/favorites/${props.movieId}`);
       setFavorite(response.data);
-=======
-  }, [movieId]);
-
-  async function handleClick() {
-    console.log(movieId,"itemid")
-    setLoading(true);
-    try {
-      const response = await localStorage.setItem('movies',JSON.stringify( movieId));
-      // setFavorite(response.data.isFavorite);
->>>>>>> 7b15585d5ca8af2713bf6cc3b7efcea227d1c2cc
     } catch (error) {
       console.error(error);
     }
